08d6fb23a20c214b88d57d9a6cc834c22d1039fb
Chad Harrington <chad@deercreeklabs.com>
2017-09-19 18:59:24 -0600

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject deercreeklabs/async-utils "0.1.0"
+(defproject deercreeklabs/async-utils "0.1.1-SNAPSHOT"
   :description
   "Utilities for async programming and testing in Clojure & Clojurescript"
   :url "http://www.deercreeklabs.com"
