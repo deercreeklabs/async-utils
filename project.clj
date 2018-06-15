53e8aeb4236a00b2062b14bc6b21b6a992d1716c
Chad Harrington <chad@deercreeklabs.com>
2018-06-15 17:12:52 -0600

    lein-release plugin: preparing 0.1.10 release

--- project.clj
+++ project.clj
@@ -37,7 +37,7 @@
              :compiler compiler}
       node-test? (assoc :notify-command ["node" output-to]))))
 
-(defproject deercreeklabs/async-utils "0.1.10-SNAPSHOT"
+(defproject deercreeklabs/async-utils "0.1.10"
   :description
   "Utilities for async programming and testing in Clojure & Clojurescript"
   :url "http://www.deercreeklabs.com"
